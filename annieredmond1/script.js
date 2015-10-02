790fc900286b5dcae1fbdfa4b38ba3610f7cee8a
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-10-01 18:04:31 -0700

    link to homework day 4

--- annieredmond1/script.js
+++ annieredmond1/script.js
@@ -2,10 +2,19 @@ $(document).ready(function() {
 	$('.one').on('click', function() {
 		$('.dog').hide("explode");
 	});
-	$('.two').on('click', function() {
-		$('.dog').animate({opacity: 0}, 1000);
-	});
+	// $('.two').on('click', function() {
+	// 	$('.dog').animate({opacity: 0}, 1000);
+	// });
 	$('.three').on('click', function() {
 		$('.dog').fadeOut();
 	});
+	$('.two').on('click', function() {
+        $('.dog').animate({
+        width: "70%",
+        opacity: 0.4,
+        marginLeft: "0.6in",
+        fontSize: "3em",
+        borderWidth: "10px"
+      }, 1500) ;
+    });
 });
