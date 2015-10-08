65f33d89fcde0174edbcd0652ce24531cdd06813
ekingan <ekingan@gmail.com>
2015-10-08 16:26:26 -0700

    add events_lab, after git submodule debacle

---
+++ ekingan/events_lab/click/base.js
@@ -0,0 +1,9 @@
+console.log("Sanity Check: JS is working!");
+
+$(document).ready(function(){
+	$('span').click(function () {
+			var clicked = $(this).html();
+				$('ul').append('<li>'  + clicked + '</li>');
+	});
+
+});
