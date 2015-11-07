fdba693509996fcfb0e80a305639e5491deff724
noel-arce <n.arcellana.00@gmail.com>
2015-11-07 15:27:49 -0800

    form and show page

--- noel-arce/week_6/w06-d5/bog_app/app/controllers/creatures_controller.rb
+++ noel-arce/week_6/w06-d5/bog_app/app/controllers/creatures_controller.rb
@@ -5,4 +5,25 @@ class CreaturesController < ApplicationController
 		render :index # render index view file (it will have access to instance variables)
 	end
 
+	def new # NEW creature form
+		@creature = Creature.new # sets @creature to a new instance of a Creature
+	  render :new # optional; this is the default behavior
+	end
+
+	def create # CREATE new creature in db
+	  creature_params = params.require(:creature).permit(:name, :description) # validate params and save them as a variable
+	  creature = Creature.new(creature_params) # create a new creature with those params
+	  if creature.save # check that it saved
+    	redirect_to creature # if saved, redirect to route that shows just this creature
+    		# ^ same as redirect_to creature_path(creature)
+    		# ^ same as redirect_to "/creatures/#{creature.id}"
+	  end
+	end
+
+	def show #SHOW a single creature
+		id = params[:id] # get the id parameter from the url and save as 'id'
+		@creature = Creature.find(id) # find the creature with that 'id' and save to an instance variable(@creature)
+		render :show # render the show page -- it will have access to the @creature variable
+	end
+
 end
--- noel-arce/week_6/w06-d5/bog_app/config/routes.rb
+++ noel-arce/week_6/w06-d5/bog_app/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'creatures#index' # GET ('/')root route
-  resources :creatures, only: [:index] # equivalent to: get "/creatures", to: "creatures#index"
+  
+  resources :creatures, only: [:index, :new, :create, :show] 
+    # ':index' equivalent to: get "/creatures", to: "creatures#index"
+    # ':new' equivalent to: get "/creatures/new", to: "creatures#new", as: "new_creature"
+    # ':creature' equivalent to: post "/creatures", to: "creatures#create"
+    # ':show' equivalent to: get "/creatures/:id", to: "creatures#show", as: "creature"
 
 end
