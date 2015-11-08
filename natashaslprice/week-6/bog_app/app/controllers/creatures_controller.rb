b85b9a4caa72ec600ead68804f5169cdacbe938d
natashaslprice <natashaslprice@gmail.com>
2015-11-07 16:10:22 -0800

    update, destroy

--- natashaslprice/week-6/bog_app/app/controllers/creatures_controller.rb
+++ natashaslprice/week-6/bog_app/app/controllers/creatures_controller.rb
@@ -12,16 +12,43 @@ class CreaturesController < ApplicationController
 
   # using params from form, create new creature and redirect to homepage
   def create
-  	Creature.create(creature_params)
-  	redirect_to('/')
+  	Creature.new(creature_params)
+  	# if created and saved, redirect
+  	if creature.save
+			redirect_to('/')
+		end
   end
 
   # show one creature, based on that creatures id
   def show
+  	# find creature to show by id
   	@creature = Creature.find(params[:id])
   end
 
-  def 
+  # show edit creature page, based on that creatures id
+  def edit 
+  	@creature = Creature.find(params[:id])
+  end
+
+	# using params from form, update creature and redirect to show creature page
+	def update
+		# find creature to update by id
+		creature = Creature.find(params[:id])
+		# updated creature with new params from form
+		creature.update_attributes(creature_params)
+		# redirect to creature show page
+		redirect_to creature
+	end
+
+	# destroy a creature based on id
+	def destroy
+		# find creature to destroy by id
+		creature = Creature.find(params[:id])
+		# destroy creature
+		creature.destroy
+		# redirect to index
+		redirect_to('/')
+	end
 
   private
 
