14d9bccaa64a05faa2d6f01ab23b73e5424b4188
Ling Lu <jojoucsd@gmail.com>
2015-11-08 02:30:54 -0800

    basic goal of bog complete

--- Jojoucsd/bog_app/app/controllers/creatures_controller.rb
+++ Jojoucsd/bog_app/app/controllers/creatures_controller.rb
@@ -25,4 +25,25 @@ class CreaturesController < ApplicationController
 		@creature = Creature.find(id)
 		render :show
 	end
+
+	def edit
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :edit
+	end
+
+	def update
+		id = params[:id]
+		creature = Creature.find(id)
+		update_attributes = params.require(:creature).permit(:name, :description)
+		creature.update_attributes(update_attributes)
+		redirect_to creature
+	end
+
+	def destroy
+		id = params[:id]
+		creature = Creature.find(id)
+		creature.destroy
+		redirect_to creatures_path
+	end
 end
